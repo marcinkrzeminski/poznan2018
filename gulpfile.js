3abd82c454a4eb0907c3254239f084317feec708
Marcin Krzeminski <marcin@krzmeminski.net>
2018-03-24 18:51:02 +0100

    CSS cleanup tools

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,17 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var csscomb = require('gulp-csscomb');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function() {
   return gulp.src('src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
-      browsers: ['last 3 versions']
+      browsers: ['last 3 versions', 'iOS 8']
     }))
+    .pipe(csscomb())
+    .pipe(cleanCSS())
     .pipe(gulp.dest('./'))
     .pipe(browserSync.stream());
 });
